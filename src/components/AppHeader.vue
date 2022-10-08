db0f93292bce2c2e28b83f64b7ae6a5d0040af57
BrotherZin <ahj13k@naver.com>
2022-10-08 19:03:26 +0900

    profile 추가

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="header">
     <v-layout align-center>
-      <div class="logo">안형진의 포트폴리오</div>
+      <div class="logo" justify-center align-center>
+        An Hyeong Jin Portfolio
+      </div>
       <v-spacer></v-spacer>
-      <v-btn text>프로젝트1</v-btn>
-      <v-btn text>프로젝트2</v-btn>
-      <v-btn text>프로젝트3</v-btn>
+      <v-btn color="#eee" text>project 1</v-btn>
+      <v-btn color="#eee" text>project 2</v-btn>
+      <v-btn color="#eee" text>project 3</v-btn>
     </v-layout>
   </div>
 </template>
@@ -14,9 +16,11 @@
 .header {
   padding: 20px;
   box-shadow: 0 6px 6px #eee;
+  background-color: black;
 }
 .header .logo {
   font-size: 20pxl;
   font-weight: bold;
+  color: #eee;
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,7 +5,7 @@
         data-aos="fade-in"
         data-aos-duration="1000"
         class="slide"
-        style="background-image: url(image/top1.jpg)"
+        style="background-image: url(image/mm2.jpg)"
       >
         <v-layout
           data-aos="fade-up"
@@ -14,10 +14,27 @@
           align-center
           justify-center
         >
-          <div class="slog">명지전문대학 안형진</div>
+          <div class="slog" data-aos="fade-up" data-aos-delay="550">
+            <br />
+            『 언제나 밝게 비춰주는 안형진입니다. 』
+            <vue-typer
+              class="slog"
+              :text="['『 언제나 밝게 비춰주는 안형진입니다. 』', '']"
+              :repeat="Infinity"
+              :shuffle="false"
+              initial-action="typing"
+              :pre-type-delay="70"
+              :type-delay="70"
+              :pre-erase-delay="2000"
+              :erase-delay="250"
+              erase-style="select-all"
+              :erase-on-complete="false"
+              caret-animation="smooth"
+            ></vue-typer>
+          </div>
         </v-layout>
       </swiper-slide>
-      <swiper-slide class="slide" style="background-image: url(image/top1.jpg)">
+      <swiper-slide class="slide" style="background-image: url(image/top3.jpg)">
         <v-layout fill-height align-center justify-center>
           <div class="slog">언제나 열심히</div>
         </v-layout>
@@ -30,11 +47,29 @@
     </swiper>
 
     <div class="section">
-      <div class="header">프로젝트 목록</div>
+      <div class="header" data-aos="fade-down" data-aos-delay="100">
+        안형진을 소개합니다.
+      </div>
       <v-layout wrap>
-        <v-flex xs4 class="pa-2" data-aos="fade-right">
+        <div style="float: left">
+          <img src="introduce/s1.jpg" class="image" />
+          <div style="float: right">
+            <p class="profilehead">Name</p>
+            안형진
+            <p class="profilehead">Birthday</p>
+            1996.06.20
+            <p class="profilehead">E-mail</p>
+            ahj13k@naver.com
+            <p class="profilehead">Address</p>
+            경기도 수원시 장안구
+            <p class="profilehead">Phone</p>
+            010 - 2061 - 6682
+          </div>
+        </div>
+
+        <!-- <v-flex xs4 class="pa-2" data-aos="fade-right">
           <v-responsive :aspect-ratio="1 / 1">
-            <img src="image/top1.jpg" class="image" />
+            <img src="introduce/s1.jpg" class="image" />
           </v-responsive>
         </v-flex>
         <v-flex xs4 class="pa-2" data-aos="fade-down" data-aos-delay="100">
@@ -51,8 +86,8 @@
           <v-responsive :aspect-ratio="1 / 1">
             <img src="image/top3.jpg" class="image" />
           </v-responsive>
-        </v-flex>
-        <v-flex xs4 class="pa-2" data-aos="fade-up" data-aos-delay="100">
+        </v-flex> -->
+        <!-- <v-flex xs4 class="pa-2" data-aos="fade-up" data-aos-delay="100">
           <v-responsive :aspect-ratio="1 / 1">
             <img src="image/top2.jpg" class="image" />
           </v-responsive>
@@ -61,7 +96,7 @@
           <v-responsive :aspect-ratio="1 / 1">
             <img src="image/top1.jpg" class="image" />
           </v-responsive>
-        </v-flex>
+        </v-flex> -->
       </v-layout>
     </div>
   </div>
@@ -72,6 +107,8 @@ import { Swiper, SwiperSlide } from "vue-awesome-swiper";
 import "swiper/swiper-bundle.css";
 import AOS from "aos";
 import "aos/dist/aos.css";
+import { VueTyper } from "vue-typer";
+
 export default {
   name: "home",
   data() {
@@ -101,9 +138,9 @@ export default {
     background-size: cover;
     background-position: center center;
     .slog {
-      font-size: 60px;
-      font-weight: bold;
-      color: white;
+      font-size: 30px;
+      font-weight: normal;
+      color: yellow;
     }
   }
 }
@@ -111,15 +148,20 @@ export default {
 .section {
   width: 1190px;
   margin: 0 auto;
+
   .header {
     text-align: center;
     font-size: 40px;
     font-weight: bold;
   }
   .image {
-    width: 100%;
+    width: 70%;
     height: 100%;
     object-fit: cover;
+  }
+  .profilehead {
+    font-weight: bold;
+    font-size: 1.8em;
   }
 }
 </style>
